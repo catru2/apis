6b497198767d034c3ff2fef5983bd47fa3364edb
catru2 <camachotrujillomartinx@.com>
2023-11-15 20:53:51 -0600

    apismulti

---
+++ src/index.js
@@ -0,0 +1,23 @@
+require("dotenv").config()
+require("./configs/ConfiDB")
+const express = require("express");
+const app= express();
+const PORT= process.env.PORT
+const userRoute=require("./routes/usuario.route")
+const pinturaRoute=require("./routes/pinturas.routes")
+const cors = require('cors')
+const authRoutes= require("./routes/auth.routes")
+app.use(cors())
+app.use(express.json())
+
+
+app.use("/api",userRoute,authRoutes,pinturaRoute)
+
+
+
+
+
+
+app.listen(PORT,() => {
+console.log(`El servidor esta funcionando en el puerto ${PORT}`);
+});
